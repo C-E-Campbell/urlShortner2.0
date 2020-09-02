19ba5cb1bf0d4d4d27bd8903de483cbe1f6af4c4
Charles <codelyfewithcharlie@gmail.com>
2020-09-02 06:38:15 -0400

    updates

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Shortner from '../Shortener/Shortner.jsx';
+function App() {
+  return (
+    <div>
+      <Shortner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Shortener/Shortner.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import style from './Shortner.module.scss';
+import axios from 'axios';
+import cx from 'classnames';
+export default function Shortner() {
+  const [link, setLink] = useState('');
+  const [isEmpty, setEmpty] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!link) {
+      setEmpty(true);
+    } else {
+      axios
+        .post('https://rel.ink/api/links/', {
+          url: link,
+        })
+        .then((res) => {
+          alert(`https://rel.ink/${res.data.hashid}`);
+          setLink('');
+        });
+    }
+  };
+
+  const handleChange = (e) => {
+    setLink(e.target.value);
+    setEmpty(false);
+  };
+
+  return (
+    <div className={style.formContainer}>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <input
+          onClick={() => setEmpty(false)}
+          className={cx(style.input, { [style.error]: isEmpty })}
+          value={link}
+          onChange={(e) => handleChange(e)}
+          type='text'
+          placeholder='Shorten a link here...'
+        />
+        <button className={style.button} type='submit'>
+          Shorten It!
+        </button>
+      </form>
+      {isEmpty ? <h5 className={style.h5}>Please add a link</h5> : null}
+    </div>
+  );
+}
